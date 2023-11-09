81a089b89c77cf390ab55eed26b6cff9440bfa01
Semih <85266757+PSEMO@users.noreply.github.com>
2023-11-09 21:59:01 +0300

    Update Program.cs

--- Galton Board/Program.cs
+++ Galton Board/Program.cs
@@ -1,61 +1,129 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
 
-//Console.WriteLine("Hello, World!");
+var watch = Stopwatch.StartNew();//
 
-List<ball> balls = new List<ball>();
-double CurrentTry = 0;
+int ballCount = 5;
+int layerCount = 18;//18 = 171pins
 
-int passCount = 0;
+Random random = new Random();
 
-while(true)
+Task[] tasks = new Task[40];
+ConcurrentBag<double> TryCounts = new ConcurrentBag<double>();
+
+ballCount--;
+
+for (int i = 0; i < tasks.Length; i++)
 {
-    CurrentTry++;
-
-    balls.Clear();
-    for (int i = 0; i < 10; i++)
+    tasks[i] = Task.Run(() =>
     {
-        ball temp = new ball();
-        temp.GoToBottom();
-        balls.Add(temp);
+        TryCounts.Add(CreateBallsAndFell());
+        //Console.WriteLine("Thread finished");
+        //Console.WriteLine("=============================");
+    });
+}
+
+Task.WaitAll(tasks);
+
+//int j = 0;
+//foreach (int tryCount in TryCounts)
+//{
+//    j++;
+//    Console.WriteLine("Thread" + TurnNumberToString(j) + ": " + tryCount);
+//}
+
+watch.Stop();
+Console.WriteLine($"The Execution time of the program is {watch.ElapsedMilliseconds}ms");//349ms, 375ms, 454ms, 406ms, 367ms
+
+while (true)
+{
+    Console.ReadLine();
+}
+
+//---------------------
+
+string TurnNumberToString(int input)
+{
+    string output = "";
+
+    if(input < 10)
+    {
+        output = "0" + input;
     }
-
-    int chosenVerticalPos = balls[0].verticalPos;
-    bool allEqual = true;
-
-    foreach (ball ball in balls)
+    else
     {
-        if (ball.verticalPos != chosenVerticalPos)
+        output = input + "";
+    }
+    return output;
+}
+double CreateBallsAndFell()
+{
+    double CurrentTry = 0;
+    int PassCount = 0;
+
+    while (true)
+    {
+        CurrentTry++;
+
+        ball firstBall = new ball(layerCount, random);
+        firstBall.GoToBottom();
+
+        int DecidedVerticalPos = firstBall.verticalPos;
+        bool allEqual = true;
+
+        for (int i = 0; i < ballCount; i++)
         {
-            allEqual = false;
-            break;
+            ball temp = new ball(layerCount, random);
+            temp.GoToBottom();
+            if (temp.verticalPos != DecidedVerticalPos)
+            {
+                allEqual = false;
+                break;
+            }
+        }
+
+        if (allEqual)
+        {
+            PassCount++;
+
+            Console.WriteLine("Current try: " + CurrentTry);
+            Console.WriteLine("Pass count: " + PassCount);
+            Console.WriteLine("Position: " + DecidedVerticalPos);
+            Console.WriteLine("----");
+
+            if (PassCount == 25)
+            {
+                return CurrentTry;
+            }
         }
     }
-
-    if(allEqual)
-    {
-        passCount++;
-
-        Console.WriteLine("Current try: " + CurrentTry);
-        Console.WriteLine("Pass count: " + passCount);
-
-        if(passCount == 1000)
-        {
-            break;
-        }
-    }
-    //Console.WriteLine("-----------------------------------");
 }
 
 class ball
 {
     public int verticalPos = 0;
-    int horizontalPos = 0;
+
+    int layerCount = 0;//18 = 171 pins
+    int rndNumber;
+
+    public ball(int layerCount, Random rnd)
+    {
+        this.layerCount = layerCount;
+        rndNumber = rnd.Next();
+    }
 
     public void GoToBottom()
     {
-        for (int horizontalPos = 0; horizontalPos < 5; horizontalPos++)
+        var bitArray = new BitArray(BitConverter.GetBytes(rndNumber));//3456213 = 010110010010011100101
+
+        for (int horizontalPos = 0; horizontalPos < layerCount; horizontalPos++)
         {
-            if(Random())
+            if(bitArray.Get(horizontalPos + 1))
                 verticalPos++;
             else
                 verticalPos--;
@@ -63,13 +131,5 @@ class ball
             //Console.WriteLine("horizontal: " + horizontalPos + ", vertical: " + verticalPos);
         }
         //Console.WriteLine("////////");
-    }
-    bool Random()
-    {
-        Random rnd = new Random();
-        if(rnd.Next(0, 2) == 0)
-            return true;
-        else
-            return false;
     }
 }
